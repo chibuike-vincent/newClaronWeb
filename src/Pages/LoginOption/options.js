f0de1dc55e5216c6b943a7b6b03ce07447fcf564
Learnitnigeria <learnitnigeria.com>
2022-04-29 12:29:46 +0100

    updated options

--- src/Pages/LoginOption/options.js
+++ src/Pages/LoginOption/options.js
@@ -1,11 +1,10 @@
 import React from "react";
 import "./options.css";
 import { useNavigate } from "react-router-dom";
-import doctorDefault from '../../images/login-doc.svg'
+import doctorDefault from '../../images/doctor-login.png'
 import claronPatient from '../../images/examination.png'
 import Navbar from '../../Component/Navbar/Navbar'
 import { FaArrowRight } from "react-icons/fa";
-
 
 function Options() {
 const navigate = useNavigate()
@@ -27,12 +26,12 @@ const navigate = useNavigate()
                     <div class="options-card-container">
                     <div onClick={() => setType("doctor")} class="options-card-box-history">
                     <img src={doctorDefault} className="doc-login-img" alt="" />
-                    <h4 className="login-options">Login As Doctor <FaArrowRight/> </h4>
+                    <button className="login-options">Login As Doctor <FaArrowRight/> </button>
                     </div>
                     
                     <div onClick={() => setType("patient")} class="options-card-box-history">
                     <img src={claronPatient} alt="" />
-                    <h4 className="login-options">Login As Patient <FaArrowRight/></h4>
+                    <button className="login-options">Login As Patient <FaArrowRight/></button>
                     </div>
                 </div>
                     
